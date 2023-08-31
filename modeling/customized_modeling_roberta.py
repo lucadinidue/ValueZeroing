5306a266ba8e7127f156504e42efb482505ed488
lucadinidue <luca.dini16@gmail.com>
2023-08-31 11:12:31 +0200

    added missing method in customized modeling roberta

--- modeling/customized_modeling_roberta.py
+++ modeling/customized_modeling_roberta.py
@@ -304,6 +304,11 @@ class RobertaLayerVZ(nn.Module):
 
         return outputs
 
+    def feed_forward_chunk(self, attention_output):
+        intermediate_output = self.intermediate(attention_output)
+        layer_output = self.output(intermediate_output, attention_output)
+        return layer_output
+
 class RobertaEncoderVZ(nn.Module):
     def __init__(self, config):
         super().__init__()
